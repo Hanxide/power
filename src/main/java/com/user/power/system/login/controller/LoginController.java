3f67dd2359e8b9c695f1e6cbfc9f83f3766b28e4
Hanxide <15136200812@163.com>
2016-12-14 15:12:37 +0800

    login update

--- src/main/java/com/user/power/system/login/controller/LoginController.java
+++ src/main/java/com/user/power/system/login/controller/LoginController.java
@@ -16,6 +16,7 @@ public class LoginController {
         System.out.println("initAaa");
         System.out.println("initAaa");
         System.out.println("initAaa");
+        System.out.println("initAaa");
         return "aaa";
     }
 }
